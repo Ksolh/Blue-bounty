0504fe953a3b6ad08508709e5ad20b3f76bbb0fc
Thomas C <carrt313@gmail.com>
2020-12-15 14:40:52 -0500

    Backend component of sharing/collab

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -6,12 +6,19 @@ module.exports = async app => {
         const { uid } = req.params;
         if (!uid) res.status(400).send('No UID Provided')
         let errored = false;
-        let notes = await sqlHelper.query(`SELECT * FROM notes WHERE authorid = '${uid}'`)
+        let allNotes = await sqlHelper.query(`SELECT * FROM notes`)
             .catch(err => {
                 errored = true
                 res.status(400).send(err)
             })
         if (errored) return
+
+        let notes = []
+        for (let i of allNotes) {
+            if (i.authorid == uid) notes.push(i)
+            else if (i.shared.split(',').includes(uid)) notes.push(i)
+        }
+
         return res.status(200).send(notes);
     });
 
@@ -48,13 +55,33 @@ module.exports = async app => {
 
     app.delete(`/api/notes/delete/:id`, async (req, res) => {
         const { id } = req.params;
-
         await sqlHelper.query(`DELETE FROM notes WHERE id = '${id}'`)
-
         return res.status(202).send({
             error: false
         })
-
     })
+
+    app.put(`/api/notes/edit/collaborators`, async (req, res) => {
+        let errored = false;
+        const { noteID, collaboratorString } = req.body;
+        let collabs = ''
+        let collabArray = await collaboratorString.split(',')
+
+        for (let i of collabArray) {
+            let row = await sqlHelper.query(`SELECT id FROM users WHERE username = '${i}' OR id = '${i}'`)
+            if (row && row[0] && row[0].id) collabs += row[0].id + ','
+        }
+
+        await sqlHelper.query(`UPDATE notes SET shared = '${collabs.substring(0, collabs.length - 1)}' WHERE id = '${noteID}'`)
+            .catch(err => {
+                errored = true;
+                res.status(400).send(err)
+            })
+        if (errored) return
+        return res.status(202).send({
+            error: false,
+            user: req.body
+        });
+    });
 
 }
