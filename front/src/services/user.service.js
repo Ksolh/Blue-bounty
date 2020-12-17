0ab6879c9d4bc81b3ec5e72086a59cf849d1f49b
Thomas C <carrt313@gmail.com>
2020-12-17 03:07:24 -0500

    please :^)

--- front/src/services/user.service.js
+++ front/src/services/user.service.js
@@ -1,27 +1,29 @@
 import axios from 'axios';
 
+const target = require('../settings.json')
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     add: async (firstName, lastName, userName, password) => {
         let err;
-        let res = await axios.post('/api/user/', { firstName: firstName, lastName: lastName, userName: userName, password: password })
+        let res = await axios.post(`${target.apiAddr}/api/user/`, { firstName: firstName, lastName: lastName, userName: userName, password: password })
             .catch(e => { err = e })
-            console.log(err)
+        console.log(err)
         if (err) return { errored: true, error: err }
         return res.data || {};
     },
     edit: async (firstName, lastName, userName, password) => {
         //to be implemented
-        let res = await axios.put(`/api/user/`, { firstName, lastName, userName, password })
+        let res = await axios.put(`${target.apiAddr}/api/user/`, { firstName, lastName, userName, password })
         return res.data || {};
     },
     delete: async (userName) => {
-        let res = await axios.delete(`/api/user/${userName}`);
+        let res = await axios.delete(`${target.apiAddr}/api/user/${userName}`);
         return res.data || [];
     },
     login: async (userName, password) => {
         let err
-        let res = await axios.post(`/api/login`, { userName: userName, password: password })
+        let res = await axios.post(`${target.apiAddr}/api/login`, { userName: userName, password: password })
             .catch(e => { err = e })
         if (err) return { errored: true, error: err }
         console.log(err)
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(logger({path: '/logs/backendlog.txt'}))
+app.use(logger({ path: '/logs/backendlog.txt' }))
 
 require('./routes/userRoutes')(app);
 require('./routes/noteRoutes')(app);
