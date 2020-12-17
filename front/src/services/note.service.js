247155b470cc96a16900bd5f4b242b25c9170ee3
Thomas C <carrt313@gmail.com>
2020-12-17 00:55:08 -0500

    fixed no notes loading

--- front/src/services/note.service.js
+++ front/src/services/note.service.js
@@ -5,17 +5,17 @@ export default {
     getAll: async uid => {
         let res = await axios.get(`/api/notes/${uid}`);
         for (let i of res.data) {
-            i.text = i.text.replace('\\#`', '\'')
-            i.title = i.title.replace('\\#`', '\'')
-            i.author = i.author.replace('\\#`', '\'')
+            if (i.text) i.text = i.text.replace('\\#`', '\'')
+            if (i.title) i.title = i.title.replace('\\#`', '\'')
+            if (i.author) i.author = i.author.replace('\\#`', '\'')
         }
         return res.data || [];
     },
     add: async (author, authorID, title, text) => {
         let err;
-        author = author.replace('\'', '\\#`')
-        title = title.replace('\'', '\\#`')
-        text = text.replace('\'', '\\#`')
+        if (author) author = author.replace('\'', '\\#`')
+        if (title) title = title.replace('\'', '\\#`')
+        if (text) text = text.replace('\'', '\\#`')
         let res = await axios.post('/api/notes/new', { author: author, authorID: authorID, title: title, text: text })
             .catch(e => { err = e })
         console.log(err)
@@ -24,8 +24,8 @@ export default {
     },
     edit: async (noteID, author, authorID, title, text) => {
         let err;
-        title = title.replace('\'', '\\#`')
-        text = text.replace('\'', '\\#`')
+        if (title) title = title.replace('\'', '\\#`')
+        if (text) text = text.replace('\'', '\\#`')
         let res = await axios.put('/api/notes/edit', { noteID: noteID, title: title, text: text })
             .catch(e => { err = e })
         console.log(err)
@@ -38,7 +38,7 @@ export default {
     },
     addCollaborators: async (noteID, collaboratorString) => {
         let err;
-        collaboratorString = collaboratorString.replace('\'', '\\#`').replace(' ', '')
+        if (collaboratorString) collaboratorString = collaboratorString.replace('\'', '\\#`').replace(' ', '')
         let res = await axios.put('/api/notes/edit/collaborators', { noteID: noteID, collaboratorString: collaboratorString })
             .catch(e => { err = e })
         console.log(err)
@@ -47,8 +47,8 @@ export default {
     },
     addComment: async (noteID, comment, commentAuthor) => {
         let err;
-        commentAuthor = commentAuthor.replace('\'', '\\#`')
-        comment = comment.replace('\'', '\\#`')
+        if (commentAuthor) commentAuthor = commentAuthor.replace('\'', '\\#`')
+        if (comment) comment = comment.replace('\'', '\\#`')
         let res = await axios.post('/api/notes/comment/new', { commentAuthor: commentAuthor, comment: comment, noteID: noteID })
             .catch(e => { err = e })
         console.log(err)
